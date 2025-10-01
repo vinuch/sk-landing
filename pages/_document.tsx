d98c32778f974bc948079b5248b907e181c47a46
vincent edeh <vincentedeh42@gmail.com>
2025-10-01 08:23:02 -0700

    Update _document.tsx
    fb domain verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="facebook-domain-verification" content="1eeq1blqorn3jba1zctj66qa5u3imw" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
